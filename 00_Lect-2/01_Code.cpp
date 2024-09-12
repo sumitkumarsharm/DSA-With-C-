25f728ff5a8929afd8937576ad5c45b318115535
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2024-09-12 19:25:24 +0000

    in this lacture i learn variable and syntex and i&o

---
+++ 00_Lect-2/01_Code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // cout << "Hello world" << endl;
+    // cout << "Hello world" << "\n"; 
+    // cout << "Sumit" << "\n" << "Sharma" << endl; 
+    int price;
+    cout << "enter the price : ";
+    cin >> price;
+    cout << "price is : " << price << endl;
+    return 0;
+}
---
+++ 00_Lect-2/02_Test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 10, b = 5;
+    cout << "sum = " << (a + b) << endl;
+    cout << "difference = " << (a - b) << endl;
+    cout << "Product = " << (a * b) << endl;
+    cout << "Division = " << (a / b) << endl;
+    cout << "modulo = " << (a % b) << endl;
+    return 0;
+}
